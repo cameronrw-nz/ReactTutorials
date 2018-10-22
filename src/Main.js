5e6b4d0836b2f9563cec0f745cc5530c84cd41be
Cameron Williams <cameronrw.nz@gmail.com>
2018-10-22 21:57:25 +0700

    Adding Margin to Main Content
    Adding margin to the main content of the page

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './MainDisplay/Home'
 import TicTacToe from './MainDisplay/TicTacToe'
 import DateString from './Common/DateString'
+import './styles/index.css';
 
 const Main = () => (
-  <main>
+  <main className="mainContent">
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/tictactoe' component={TicTacToe} />
